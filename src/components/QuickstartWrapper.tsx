6f0c415e71373984f4a8e8ee337756d085f06758
Giles Wells <giles@mediagearhead.com>
2020-08-06 16:06:22 -0400

    Quickstart page click Focus management

--- src/components/QuickstartWrapper.tsx
+++ src/components/QuickstartWrapper.tsx
@@ -4,6 +4,7 @@ import PageHeader from './PageHeader';
 interface IQuickstartWrapperProps {
   halo: string;
   quickstartContent: React.StatelessComponent;
+  forwardedRef?: React.RefObject<HTMLDivElement>;
 }
 
 export default function Quickstart(props: IQuickstartWrapperProps) {
@@ -11,7 +12,13 @@ export default function Quickstart(props: IQuickstartWrapperProps) {
 
   return (
     <div role="region" aria-labelledby="api-documentation">
-      <PageHeader id="api-documentation" halo={halo} header="Quickstart" />
+      <PageHeader
+        id="api-documentation"
+        halo={halo}
+        header="Quickstart"
+        tabIndex={-1}
+        forwardedRef={props.forwardedRef}
+      />
       {quickstartContent({})}
     </div>
   );
--- src/containers/documentation/QuickstartPage.tsx
+++ src/containers/documentation/QuickstartPage.tsx
@@ -6,16 +6,36 @@ import { getApiDefinitions } from '../../apiDefs/query';
 import QuickstartWrapper from '../../components/QuickstartWrapper';
 import { IApiNameParam } from '../../types';
 
-export default class QuickstartPage extends React.Component<RouteComponentProps<IApiNameParam>, {}> {
+export default class QuickstartPage extends React.Component<
+  RouteComponentProps<IApiNameParam>,
+  {}
+> {
+  public pageHeader: React.RefObject<HTMLDivElement>;
+  constructor(props: RouteComponentProps<IApiNameParam>) {
+    super(props);
+    this.pageHeader = React.createRef();
+  }
+  public componentDidMount() {
+    const element = this.pageHeader!.current;
+    setTimeout(() => {
+      element!.focus();
+    }, 0);
+  }
   public render() {
     const { apiCategoryKey } = this.props.match.params;
-    const { 
-      content: { quickstart: quickstartContent }, 
+    const {
+      content: { quickstart: quickstartContent },
       name,
     } = getApiDefinitions()[apiCategoryKey];
 
     if (quickstartContent) {
-      return <QuickstartWrapper halo={name} quickstartContent={quickstartContent} />;
+      return (
+        <QuickstartWrapper
+          halo={name}
+          quickstartContent={quickstartContent}
+          forwardedRef={this.pageHeader}
+        />
+      );
     } else {
       return <Redirect to={`/explore/${apiCategoryKey}`} />;
     }
