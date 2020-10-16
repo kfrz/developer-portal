6398ec216bdfa1bf4b25b9ec38db9dc61088cb9a
TS000 <tylersnell@me.com>
2020-10-16 12:47:14 -0600

    API 2900: Create fetch wrapper for native fetch

---
+++ src/utils/fetchWrap.ts
@@ -0,0 +1,29 @@
+import * as Sentry from "@sentry/browser";
+
+export const fetchWrap = async <T extends Record<string, unknown> | any>(request: Record<string, unknown>, errorFlag: Record<string, unknown>, dispatch: Record<string, unknown> ): Promise<T> => {
+  try {
+    const response = await fetch(request);
+    if (!response.ok && response.status !== 400) {
+      throw Error(response.statusText);
+    }
+    const json: Record<string, unknown> = await response.json();
+    if (json.errors) {
+      throw Error(`${errorFlag}: ${json.errors.join(', ')}`);
+    }
+    if (json.token || json.clientID) {
+      const result = dispatch(submitFormSuccess(json.token, json.clientID, json.clientSecret));
+      history.push('/applied');
+      return result;
+    } else {
+      return dispatch(submitFormError(json.errorMessage));
+    }
+  }
+    
+  catch (error) {
+    Sentry.withScope(scope => {
+      scope.setLevel(Sentry.Severity.fromString('warning'));
+      Sentry.captureException(error);
+    });
+    return dispatch(submitFormError(error.message));
+  }
+};
